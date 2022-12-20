3a788092b43fe2137f1f84c818e88a055d1a61bc
Alessandro Crugnola <alessandro.crugnola@swisscom.com>
2022-12-20 21:49:57 +0100

    verbosity

--- device.go
+++ device.go
@@ -51,7 +51,7 @@ func (d Device) WriteScreenCap(output *os.File) (transport.Result, error) {
 	var pb = d.Client.NewProcess()
 	pb.WithCommand("exec-out")
 	pb.WithArgs("screencap", "-p")
-	pb.Verbose(true)
+	pb.Verbose(false)
 	pb.WithStdout(output)
 	return pb.Invoke()
 }
